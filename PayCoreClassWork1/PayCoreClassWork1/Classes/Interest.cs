bf26daa5d9fcd6531e5762c87e62d042c8c8a7a3
Gökay Ü <106078471+TheGreamer@users.noreply.github.com>
2022-08-25 12:25:09 +0300

    Add files via upload

---
+++ PayCoreClassWork1/PayCoreClassWork1/Classes/Interest.cs
@@ -0,0 +1,23 @@
+﻿namespace PayCoreClassWork1.Classes
+{
+    public class Interest
+    {
+        // InterestsController'in GetInterest adlı action metodunun dönüş cevabı olarak kullanılması için oluşturulmuş yapıcı metoddur.
+        public Interest(double totalBalance, double rate)
+        {
+            // Parametreye gönderilen değerler propertylere atanır.
+            TotalBalance = totalBalance;
+            Rate = rate;
+        }
+
+        // Vade sonunda elde edilen toplam bakiye.
+        public double TotalBalance { get; set; }
+
+        // Faiz oranı.
+        public double Rate { get; set; }
+    }
+
+    // Faiz tiplerine ait hesaplama türüne ilişkin validasyonun sağlanması adına oluşturulmuş faiz tipi enum'u.
+    // Action metodunda parametre olarak bool tipi de kullanılabilirdi ancak geleceğe yönelik ihtiyaçların artması durumunda tüm ihtimalleri bünyesinde barındıran bir enum olması daha sağlıklıdır.
+    public enum InterestType { Simple, Compound }
+}
---
+++ PayCoreClassWork1/PayCoreClassWork1/Controllers/InterestsController.cs
@@ -0,0 +1,25 @@
+﻿/*---------------------*/
+/* Made By The Greamer */
+/*---------------------*/
+
+using Microsoft.AspNetCore.Mvc;
+using PayCoreClassWork1.Classes; // Faiz sınıfına ulaşabilmek için gerekli tanımlama.
+
+namespace PayCoreClassWork1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InterestsController : ControllerBase
+    {
+        // Basit ve bileşik faiz hesabı yapan metod. Ödevden farklı olarak bakiye, faiz oranı ve vade yılı parametrelerinin yanında ekstra olarak 4. parametre bulunmaktadır. Bu parametre faiz türüdür ve varsayılan değeri basit faizdir.
+        [HttpGet("GetInterest")]
+        public Interest GetInterest(double balance, double rate, int expiration, InterestType interestType = InterestType.Simple)
+        {
+            double totalBalance = interestType == InterestType.Simple // Eğer parametreden gelen faiz tipi basit faiz ise,
+                                ? balance * (1 + expiration * rate) // Vade sonu toplam bakiye değeri basit faiz formülüne göre hesaplanır.
+                                : balance * Math.Pow(1.0 + rate, expiration); // Ancak parametreden gelen faiz tipi bileşik faiz ise, vade sonu toplam bakiye değeri bileşik faiz formülüne göre hesaplanır.
+
+            return new Interest(totalBalance, rate); // Cevap olarak faiz sınıfına ait vade sonu bakiye ve faiz oranı döndürülür.
+        }
+    }
+}
